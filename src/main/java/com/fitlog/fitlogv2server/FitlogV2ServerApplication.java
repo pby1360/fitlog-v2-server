bcacfac9afb18feda89bd0efd836504f97a8c27c
pby1360 <pby1360@gmail.com>
2025-10-24 01:01:50 +0900

    Initial commit

---
+++ src/main/java/com/fitlog/fitlogv2server/FitlogV2ServerApplication.java
@@ -0,0 +1,13 @@
+package com.fitlog.fitlogv2server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitlogV2ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitlogV2ServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fitlog/fitlogv2server/FitlogV2ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitlog.fitlogv2server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitlogV2ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
