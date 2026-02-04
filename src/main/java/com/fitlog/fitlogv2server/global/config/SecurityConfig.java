753d2e89bc93fcb1d458ee72ce032f2ef018c73f
pby1360 <pby1360@gmail.com>
2026-02-05 00:56:46 +0900

    solve cors error

--- src/main/java/com/fitlog/fitlogv2server/global/config/SecurityConfig.java
+++ src/main/java/com/fitlog/fitlogv2server/global/config/SecurityConfig.java
@@ -76,7 +76,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:5173", "https://fitlog-v2-web.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
